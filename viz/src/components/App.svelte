bddd862f6ffd841a65da90623ce50b6a088b50f8
saathvikpd <saathvik.pd@gmail.com>
2024-02-13 18:22:58 -0800

    update

--- viz/src/components/App.svelte
+++ viz/src/components/App.svelte
@@ -1,11 +1,11 @@
 <script>
     import Graph from "./Graph.svelte";
 </script>
-<section class="container">
+<section class="container" transform="translate(100, 10)">
     <svelte:component this={Graph}/></section>
 <style>
     .container {
-    max-width: 640px;
+    max-width: 750px;
     border: solid 1px black;
     margin: auto;
     }
--- viz/src/components/Graph.svelte
+++ viz/src/components/Graph.svelte
@@ -1,9 +1,9 @@
 <script>
     import * as d3 from 'd3';
-    let chartWidth = 500;
-    let chartHeight = 300;
+    import { onMount } from 'svelte';
 
-    
+    let chartWidth = 700;
+    let chartHeight = 500;
 
     const paddings = {
                 top: 50,
@@ -12,59 +12,75 @@
                 bottom: 50,
             };
 
-    let nr = ['nuclear_share_elec', 'coal_share_elec', 'gas_share_elec', 'oil_share_elec'];
+    let nr = ['coal_share_elec', 'nuclear_share_elec', 'gas_share_elec', 'oil_share_elec'];
     let r = ['solar_share_elec', 'biofuel_share_elec', 'other_renewables_share_elec_exc_biofuel', 'wind_share_elec', 'hydro_share_elec'];
-
-    let url = "https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv";
+    const all_cols = r.concat(nr);
 
     let data = [];
-    d3.csv(url).then(function (d) {
-        data = d
-    })
-
     let subset = [];
 
     let xScale = d3.scaleLinear().domain(d3.extent(subset, (d) => d[nr[0]])).range([paddings.left, chartWidth - paddings.right]);
     let yScale = d3.scaleLinear().domain(d3.extent(subset, (d) => d[nr[1]])).range([paddings.top, chartHeight - paddings.bottom]);
 
+    let cumul = [];
+
+    onMount(async () => {
+
+        const res = await fetch('owid-energy-data.csv'); 
+
+        const csv = await res.text();
+
+        data = d3.csvParse(csv, d3.autoType)
+
+    });
+
+    
+    
+    
     $: {
-        if (data.length !== 0) {
-            
+
+        if (data.length > 0) {
             let country = "United States";
-            let year = 2000;
+            let year1 = 2000;
+            let year2 = 2023;
 
             let item;
             let sub_item;
 
-            let temp = nr;
-            temp.unshift("year");
+            let temp = all_cols;
 
+            let running_sum = 0;
             for (let i = 0; i < data.length; ++i) {
                 item = data[i];
-                sub_item = {};
-                if (item["country"] == country && item["year"] >= year) {
+                if (item["country"] == country && item["year"] >= year1 && item["year"] < year2) {
+                    sub_item = [item["year"]];
+                    running_sum = 0;
                     for (let j = 0; j < temp.length; ++j) {
-                        sub_item[temp[j]] = item[temp[j]];
+                        running_sum = running_sum + item[temp[j]];
+                        sub_item.push(running_sum);
+                        
                     }
                     subset.push(sub_item);
                 }
             }
 
-            
-            
-            
-            xScale = d3.scaleLinear().domain(d3.extent(subset, (d) => d[nr[0]])).range([paddings.left, chartWidth - paddings.right]);
-            yScale = d3.scaleLinear().domain(d3.extent(subset, (d) => d[nr[1]])).range([paddings.top, chartHeight - paddings.bottom]);
+            console.log(subset);
 
             
+
+            
+            
+            xScale = d3.scaleLinear().domain([year1, year2]).range([paddings.left, chartWidth - paddings.right]);
+            yScale = d3.scaleLinear().domain([100, 0]).range([paddings.top, chartHeight - paddings.bottom]);
 
 
         }
+        
 
         
     }
 
-    console.log(xScale(2005));
+    
     
 
 </script>
@@ -91,14 +107,28 @@
    <g>
    {#each subset as row, index}
    {#if index < subset.length - 1}
-        <line
-            x1={xScale(row[nr[0]])}
-            x2={xScale(subset[index + 1][nr[0]])}
-            y1={yScale(row[nr[1]])}
-            y2={yScale(subset[index + 1][nr[1]])}
-            stroke="black"
-            stroke-width="2"
-        />
+        {#each row as col, index1}
+            {#if index1 > 0 && index1 <= 4}
+                <line
+                    x1={xScale(row[0])}
+                    x2={xScale(subset[index + 1][0])}
+                    y1={yScale(col)}
+                    y2={yScale(subset[index + 1][index1])}
+                    stroke="black"
+                    stroke-width="1"
+                />
+            {/if}
+            {#if index1 > 4}
+                <line
+                    x1={xScale(row[0])}
+                    x2={xScale(subset[index + 1][0])}
+                    y1={yScale(col)}
+                    y2={yScale(subset[index + 1][index1])}
+                    stroke="red"
+                    stroke-width="1"
+                />
+            {/if}
+        {/each}
    {/if}
    {/each}
    </g>
